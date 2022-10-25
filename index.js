8737d3ce6495937461fb5a613665d5f468635bcb
Lunatik6572 <ericrodriguez4@csus.edu>
2022-10-25 13:58:30 -0700

    Updated index to add local storage example

--- index.js
+++ index.js
@@ -4,9 +4,47 @@ const observer = new IntersectionObserver((entries) => {
 		if (entry.isIntersecting)
 		{
 			entry.target.classList.add("show");
-		} else {
-			entry.target.classList.remove("show");
-		}
+		} 
+		// else {
+		// 	entry.target.classList.remove("show");
+		// }
 	})
 });
 hiddenElements.forEach((element) => {observer.observe(element)});
+
+
+// Code for bg
+const re = /\b[0-9A-F]{6}\b/gi
+const applyBgBtn = document.getElementById('apply_bg_btn');
+const resetBgBtn = document.getElementById('reset_bg_btn');
+const textAreaBg = document.getElementById('bg_color');
+applyBgBtn.addEventListener('click', () => {
+	const val = textAreaBg.value;
+	if (val.match(re))
+	{
+		let htmlDom = document.getElementsByTagName('html');
+		let bodyDom = document.getElementsByTagName('body');
+		htmlDom[0].style.backgroundColor = '#' + val;
+		bodyDom[0].style.backgroundColor = '#' + val;
+		window.localStorage.setItem('bg_color', val);
+	}
+});
+resetBgBtn.addEventListener('click', () => {
+	let htmlDom = document.getElementsByTagName('html');
+	let bodyDom = document.getElementsByTagName('body');
+	htmlDom[0].style.backgroundColor = '#131316';
+	bodyDom[0].style.backgroundColor = '#131316';
+	window.localStorage.removeItem('bg_color');
+	textAreaBg.value = '';
+})
+
+// Restore previous applied bg color
+let prev_bg;
+if (prev_bg = window.localStorage.getItem('bg_color'))
+{
+	let htmlDom = document.getElementsByTagName('html');
+	let bodyDom = document.getElementsByTagName('body');
+	htmlDom[0].style.backgroundColor = '#' + prev_bg;
+	bodyDom[0].style.backgroundColor = '#' + prev_bg;
+}
+
